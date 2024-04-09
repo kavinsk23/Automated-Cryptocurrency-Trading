d047c008322e697088560e4d986d7260cd5e3aaf
kavinsk23 <23.kavinsk@gmail.com>
2024-04-09 12:05:09 +0530

    Initial commit

--- implement.py
+++ implement.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = 'S9BCX4BJJOONO1TV'  # Your Alpha Vantage API Key
 SYMBOL = 'AAPL'  # Stock symbol, e.g., AAPL for Apple Inc.
 INTERVAL = 'daily'  # Interval could be 1min, 5min, 15min, 30min, 60min, daily, weekly, or monthly
-TIME_PERIOD = 14  # Commonly used tinme period for RSI calculation
+TIME_PERIOD = 14  # Commonly used time period for RSI calculation
 SERIES_TYPE = 'close'  # The type of price data to use (close, open, high, low)
 
 # Update the URL to use the RSI function
